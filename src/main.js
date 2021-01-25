cd3148fbbcfd4cd867d54cad724aba616d304cf8
leeokhee <pixellog@gmail.com>
2021-01-25 15:11:46 +0900

    first add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import './style.scss'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 import App from './App.vue'
 
 Vue.config.productionTip = false
